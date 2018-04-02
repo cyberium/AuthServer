cebb896751f896bcecac4eb2a9b93922dfebc58b
cyberium <cyberium@users.noreply.github.com>
2018-04-02 17:16:13 +0200

    First commit of AuthServer, Realmd rewritted.
    Copying framework and rearanging everything.
    Make OpenSSL and Mysql extern.

---
+++ cmake/options.cmake
@@ -0,0 +1,26 @@
+option(CMOPT_DEBUG         "Include additional debug-code in core" OFF)
+option(CMOPT_WARNINGS      "Show all warnings during compile"      OFF)
+option(CMOPT_PCH           "Use precompiled headers"               ON)
+
+# TODO: options that should be checked/created:
+#option(CLI                  "With CLI"                              ON)
+#option(RA                   "With Remote Access"                    OFF)
+#option(SQL                  "Copy SQL files"                        OFF)
+#option(TOOLS                "Build tools"                           OFF)
+
+message("")
+message(STATUS
+  "This script builds the MaNGOS server.
+  Options that can be used in order to configure the process:
+    CMAKE_INSTALL_PREFIX    Path where the server should be installed to
+    CMOPT_PCH               Use precompiled headers
+    CMOPT_DEBUG             Include additional debug-code in core
+    CMOPT_WARNINGS          Show all warnings during compile
+
+  To set an option simply type -D<OPTION>=<VALUE> after 'cmake <srcs>'.
+  Also, you can specify the generator with -G. see 'cmake --help' for more details
+  For example:
+    Build realm deamon server
+    cmake -DCMAKE_INSTALL_PREFIX=../opt/cmangos .."
+)
+message("")
---
+++ cmake/showoptions.cmake
@@ -0,0 +1,51 @@
+# output generic information about the core
+message(STATUS "CMaNGOS-Core revision : ${GIT_REVISION}")
+message(STATUS "Install server to     : ${CMAKE_INSTALL_PREFIX}")
+
+# Show infomation about the options selected during configuration
+
+# if(CLI)
+#   message(STATUS "Build with CLI        : Yes (default)")
+#   add_definitions(-DENABLE_CLI)
+# else()
+#   message(STATUS "Build with CLI        : No")
+# endif()
+
+# if(RA)
+#   message(STATUS "* Build with RA       : Yes")
+#   add_definitions(-DENABLE_RA)
+# else(RA)
+#   message(STATUS "* Build with RA       : No  (default)")
+# endif(RA)
+
+if(CMOPT_PCH)
+  message(STATUS "Use PCH               : Yes (default)")
+else()
+  message(STATUS "Use PCH               : No")
+endif()
+
+if(CMOPT_DEBUG)
+  message(STATUS "Build in debug-mode   : Yes")
+else()
+  message(STATUS "Build in debug-mode   : No  (default)")
+endif()
+
+if(CMOPT_WARNINGS)
+  message(STATUS "Show all warnings     : Yes (default)")
+else()
+  message(STATUS "Show all warnings     : No")
+endif()
+
+# if(SQL)
+#   message(STATUS "Install SQL-files     : Yes")
+# else()
+#   message(STATUS "Install SQL-files     : No  (default)")
+# endif()
+
+# if(TOOLS)
+#   message(STATUS "Build map/vmap tools  : Yes")
+# else()
+#   message(STATUS "Build map/vmap tools  : No  (default)")
+# endif()
+
+message("")
